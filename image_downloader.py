b703f5fa3d9a90b811bc30301749838a163eb8e0
Giacomo Ferretti <me@hexile.xyz>
2019-06-04 18:10:58 +0200

    Added image downloader

---
+++ image_downloader.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright 2019 Giacomo Ferretti
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import json
+import os
+
+import requests
+from mcdapi import endpoints
+
+__proxy_enabled__ = True
+__proxy_url__ = 'socks5://127.0.0.1:9050'
+__image_folder__ = 'images'
+__offers_file__ = 'offers_parsed.json'
+
+
+def main():
+    if not os.path.isdir(__image_folder__):
+        os.mkdir(__image_folder__)
+
+    session = requests.session()
+
+    if __proxy_enabled__:
+        session.proxies = {
+            'http': __proxy_url__,
+            'https': __proxy_url__
+        }
+
+    # Load offers
+    with open(__offers_file__) as f:
+        offers = json.loads(f.read())
+
+    for x in offers:
+        params = endpoints.PROMO_IMAGE['params']
+        params['path'] = x['promoImagePath']
+        params['imageFormat'] = 'png'
+        r = session.request(endpoints.PROMO_IMAGE['method'], endpoints.PROMO_IMAGE['url'].format(size=512),
+                            params=params)
+
+        if r.status_code == 200:
+            with open(os.path.join(__image_folder__, x['promoImagePath']), 'wb') as f:
+                f.write(r.content)
+
+        """
+        if r.status_code == 200:
+            # Create inline keyboard
+            keyboard = [
+                [
+                    InlineKeyboardButton('\u2705 Va bene', callback_data='{}_id_{}'.format(self.name, id_)),
+                    InlineKeyboardButton('\u2b05 Torna indietro', callback_data='{}_list'.format(self.name))
+                ]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+    
+            body = self.__config__.get_template('coupon_preview.html') \
+                .format(title=offers[offer_index]['title'], description=offers[offer_index]['description'])
+    
+            context.bot.send_photo(chat_id=query.message.chat.id, photo=BytesIO(r.content), caption=body,
+                                   parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+        """
+
+if __name__ == '__main__':
+    main()
