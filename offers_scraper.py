91dfe102db6c483a5592d6cb76ebd1b0afbc4067
supersupersnivy97 <supersupersnivy97.new@gmail.com>
2019-06-23 18:26:42 +0200

    ..

--- offers_scraper.py
+++ offers_scraper.py
@@ -28,12 +28,9 @@ __end_id__ = 20000
 
 def main():
     non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-    
     print('Starting scraping {num} offers using mcdapi ({version})...'.format(num=(__end_id__ - __start_id__ + 1),
                                                                               version=mcdapi.__version__))
-
     start_time = datetime.now()
-
     session = requests.session()
 
     if __proxy_enabled__:
